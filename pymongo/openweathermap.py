c7a358b72e24b8a95b3243b2c629c21cd41479ac
hethune <wenhangreg@gmail.com>
2014-03-04 16:40:31 -0800

    More robust structure

--- pymongo/openweathermap.py
+++ pymongo/openweathermap.py
@@ -5,27 +5,59 @@ import math
 from pymongo import MongoClient
 from pymongo import ASCENDING, DESCENDING
 
+def debug(info):
+    print info
+
+def log(info):
+    print info
 
 def parseJson(url):
-    data = json.load(urllib2.urlopen(url))
+    try:
+        data = json.load(urllib2.urlopen(url))
+        return data
+    except ValueError as e:
+        log(e)
+        exit()
+    except:
+        log("Url Error: " + url)
+        exit()
+
+def openDBCollection(database, collectionName):
+    client = MongoClient()
+    db = client[database]
+    collection = db[collectionName]
+    # In case we need to make results unique
+    # collection.ensure_index([("name", ASCENDING), ("start", ASCENDING)], unique=True, dropDups=True)
+    return collection
+
+def validateData(raw):
+    data = [];
+    for key in raw:
+        value = raw[key]
+        if isinstance(value, basestring) and value.lower() == "error":
+            log("Failed retrieve latency for " + key)
+        else:
+            value["name"] = key
+            data.append(value)
     return data
 
-
-def write(post):
-    client = MongoClient()
-    db = client.weather
-
-    posts = db.posts
-    posts.ensure_index([("name", ASCENDING), ("day", ASCENDING)], unique=True, dropDups=True)
-    try:
-        post_id = posts.insert(post)
-        print post_id
-    except Exception:
-        print "Duplicate key"
-        return False
+def write(collection, posts):
+    for post in posts:
+        try:
+            post_id = collection.insert(post)
+            debug(post_id)
+        except Exception:
+            log("Insertion failed for" + post["name"])
     return True
 
+def main(url):
+    # url = "http://stackoverflow.com/questions/1479776/too-many-values-to-unpack-exception"
+    data = parseJson(url)
+    posts = validateData(data)
+    collection = openDBCollection('latency', 'dmos')
+    write(collection, posts)
+
 url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk"
-post = parseJson(url)
-post['day'] = math.floor(time.time() % 86400)
-write(post)
+main(url)
+
+
